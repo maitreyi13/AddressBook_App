a1683b97e4865b6f399d988d714679f2759ba153
Maitreyi Kulkarni <107098811+maitreyi13@users.noreply.github.com>
2022-09-05 20:01:39 +0530

    Added: Get Data From local Storage

--- js/AddressBook.js
+++ js/AddressBook.js
@@ -1,71 +1,81 @@
-class AddressBookContact{
+class AddressBook{
 
-  constructor(id,name,phone,address,city,state,zip){
-      this._id;
-      this.name;
-      this.phone;
-      this.address;
-      this.city;
-      this.state;
-      this.zip;
-  }
-  get id(){
-    return this._id;
-  }
+    get id()
+    {
+        return this._id;
+    }
 
-  set id(id) {
-    this._id = id;
-  } 
-  get name() {
-      return this._name;
-  }
+    set id(id)
+    {
+        this._id=id;
+    }
 
-  set name(name) {
-      this._name = name;
-  }
+    get name() {
+        return this._name;
+    }
+    set name(name) {
+        let nameRegex=RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$')
+        if(nameRegex.test(name)){
+            this._name=name;
+        }
+        else{
+            throw "Name Is Incorrect!"
+        }
+    }
 
-  get phone() {
-      return this._phone;
-  }
+    get phone() {
+        return this._phone;
+    }
 
-  set phone(phone) {
-      this._phone = phone;
-  }
+    set phone(phone) {
+        let phonergx=RegExp('^[+]{0,1}[0-9]{2}\\s{0,1}[0-9]{10}$')
+        if(phonergx.test(phone)){
+            this._phone=phone;
+        }else throw "Phone Number Is Incorrect!"
+    }
 
-  get address() {
-      return this._address;
-  }
+    get city(){
+        return this._city;
+    }
 
-  set address(address) {
-      this._address = address;
-  }
+    set city(city){
+        this._city=city;
+    }
 
-  get city() {
-      return this._city;
-  }
+    get state(){
+        return this._state;
+    }
 
-  set city(city) {
-      this._city = city;
-  }
+    set state(state){
+        this._state=state;
+    }
 
-  get state() {
-      return this._state;
-  }
+    get address(){
+        return this._address;
+    }
 
-  set state(state) {
-      this._state = state;
-  }
+    set address(address){
+        let addrrgx = RegExp('.*')
+        if(addrrgx.test(address)){
+            this._address = address;
+        }else throw "Address Is Incorrect!";
+    }
 
-  get zip() {
-      return this._zip;
-  }
+    get zip(){
+        return this._zip;
+    }
 
-  set zip(zip) {
-      this._zip = zip;
-  }
+    set zip(zip){
+        let ziprgx=RegExp('^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$')
+        if(ziprgx.test(zip)){
+            this._zip = zip;
+        }else throw "Zip Code Is Incorrect!";     
+    }
+    
+    toString() {
+        return "Id="+this.id +  "name=" + this.name + ", Phone No=" + this.phone +
+        ", Address=" + this.address + ", Zip Code=" + this.zip +
+        ", City=" + this.city + ", State=" +this.state;
+    }
 
-  toString() {
-      return 'Name = ' + this.name + ", Phone = " + this.phone + ", Address = " + this.address
-          + ", City = " + this.city + ", State = " + this.state + ", Zip Code = " + this.zip;
-  }
 }
